f665f8a0461fc9fe8d580795993c1de83aee2a61
Kupchik Vitaliy <86719586+KupchykVitaly@users.noreply.github.com>
2023-11-19 16:42:07 +0200

    Add files via upload

---
+++ lab6/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(txtCatet1.Text, out double catet1) && double.TryParse(txtCatet2.Text, out double catet2))
+            {
+                double hypotenuse = Math.Sqrt(catet1 * catet1 + catet2 * catet2);
+                double area = 0.5 * catet1 * catet2;
+
+                txtHypotenuse.Text = hypotenuse.ToString("F2");
+                txtArea.Text = area.ToString("F2");
+            }
+            else
+            {
+                MessageBox.Show("Введіть коректні значення для катетів.");
+            }
+
+        }
+
+    }
+}

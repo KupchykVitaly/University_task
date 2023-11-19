9b09cf3e61e585c6e7828cd64dee3348331a27c7
Kupchik Vitaliy <86719586+KupchykVitaly@users.noreply.github.com>
2023-11-19 17:04:12 +0200

    Add files via upload

---
+++ lab3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+using WindowsFormsApp2;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Graphics graph;
+        public Pen pen1, pen2, pen3;
+        private Star lastStar; // Змінна для зберігання останньої створеної зірки
+        public int scale = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graph = CreateGraphics();
+            pen1 = new Pen(Color.Black);
+            pen2 = new Pen(Color.White);
+            pen3 = new Pen(Color.Red);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Point p1 = new Point(scale * (Star.StarCount * 10 + 10), scale * 30);
+            Star star = new Star(p1, 10, 4, this);
+            if (lastStar != null)
+            {
+                star.IncreaseSize(lastStar); // Збільшення розміру з урахуванням останньої зірки
+            }
+            star.Show();
+            lastStar = star; // Зберігаємо останню створену зірку
+        }
+    }
+}
---
+++ lab3/Gear1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public class Star
+    {
+        public static int StarCount = 0;
+        private Point center;
+        private int radius;
+        private Form1 form;
+        private double angle = 0;
+        private double spikeLength;
+        private int spikeCount;
+        private int lastRadius = 10; // Зберігаємо розмір останньої створеної зірки
+        public double Angle { get { return angle; } }
+
+        public Star(Point c, double r, int spikes, Form1 form1)
+        {
+            ++StarCount;
+            center = c;
+            radius = form1.scale * (int)r;
+            form = form1;
+            spikeLength = radius * 0.2;
+            spikeCount = spikes;
+        }
+
+        public class FourPointedStar
+        {
+            private int a; // Параметр a
+            private int b; // Параметр b
+            private Form1 form;
+
+            public FourPointedStar(int a, int b, Form1 form1)
+            {
+                this.a = a;
+                this.b = b;
+                form = form1;
+            }
+
+            public void Draw(Pen pen, Graphics graph)
+            {
+                if (graph != null)
+                {
+                    Point[] starPoints = new Point[8];
+                    double angle = Math.PI / 4; // Кут для розташування точок
+                    double step = 2 * Math.PI / 8; // Крок між точками
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        double x = form.ClientSize.Width / 2 + (a * Math.Cos(angle));
+                        double y = form.ClientSize.Height / 2 + (b * Math.Sin(angle));
+                        starPoints[i] = new Point((int)x, (int)y);
+                        angle += step;
+                    }
+
+                    graph.DrawPolygon(pen, starPoints);
+                }
+            }
+
+        }
+        public void Show() { Draw(form.pen1, form.graph); }
+        public void Hide() { Draw(form.pen2, form.graph); }
+    
+    public void IncreaseSize(Star lastStar)
+        {
+            radius = (int)(lastStar.radius * 1.1); // Збільшуємо розмір зірки на 10%
+            spikeLength = radius * 0.2; // Оновлюємо розмір кінців
+        }
+    }
+}

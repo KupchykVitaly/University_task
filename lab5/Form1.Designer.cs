61c174d8c1643c730f3adccfd20e92294476d78e
Kupchik Vitaliy <86719586+KupchykVitaly@users.noreply.github.com>
2023-11-19 16:40:29 +0200

    Add files via upload

---
+++ lab5/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Малювати";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(372, -54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 426);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(252, 37);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(252, 63);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(191, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ширина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Висота";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "XML-Сериалізація";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "XML-Десереалізація";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 121);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Бінарна Сериалізація";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(143, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Бінарна Десереалізація";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 179);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(143, 186);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ lab5/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        XmlSerializer xmlSerial;
+        FileStream fs;
+        SandClock sandclock;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            Type type = typeof(SandClock);
+            MethodInfo[] mi = type.GetMethods();
+            foreach (MethodInfo m in mi)
+            {
+                listBox1.Items.Add(m.ToString());
+            }
+        }
+
+        [Serializable]
+        public class SandClock
+        {
+            private double a;
+            private double b;
+
+            public double A { get { return a; } set { a = value; } }
+            public double B { get { return b; } set { b = value; } }
+
+            public SandClock()
+            {
+                a = 0;
+                b = 0;
+            }
+
+            public SandClock(double _a, double _b)
+            {
+                a = _a;
+                b = _b;
+            }
+
+            public void Draw(Graphics g, double x, double y)
+            {
+                g.Clear(Color.Empty);
+                Pen pn = new Pen(Color.Black, 5); // Змінено товщину контуру на 2
+
+                PointF[] topTriangle = setTopTrianglePoints(x, y);
+                PointF[] bottomTriangle = setBottomTrianglePoints(x, y);
+
+                g.DrawPolygon(pn, topTriangle);
+                g.DrawPolygon(pn, bottomTriangle);
+                g.FillPolygon(new SolidBrush(Color.White), topTriangle);
+                g.FillPolygon(new SolidBrush(Color.White), bottomTriangle);
+            }
+
+
+            public PointF[] setTopTrianglePoints(double x, double y)
+            {
+                PointF[] points = new PointF[3];
+                points[0] = new PointF((float)(x - a / 2), (float)(y - b / 2));
+                points[1] = new PointF((float)(x + a / 2), (float)(y - b / 2));
+                points[2] = new PointF((float)x, (float)(y));
+                return points;
+            }
+
+            public PointF[] setBottomTrianglePoints(double x, double y)
+            {
+                PointF[] points = new PointF[3];
+                points[0] = new PointF((float)(x - a / 2), (float)(y + b / 2));
+                points[1] = new PointF((float)(x + a / 2), (float)(y + b / 2));
+                points[2] = new PointF((float)x, (float)(y));
+                return points;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sandclock = new SandClock(double.Parse(textBoxA.Text), double.Parse(textBoxB.Text));
+            double x = pictureBox1.ClientSize.Width / 2.0;
+            double y = pictureBox1.ClientSize.Height / 2.0;
+            if (sandclock != null)
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    sandclock.Draw(g, x, y);
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            sandclock = new SandClock(double.Parse(textBoxA.Text), double.Parse(textBoxB.Text));
+            xmlSerial = new XmlSerializer(typeof(SandClock));
+            fs = new FileStream("sandclock.xml", FileMode.OpenOrCreate);
+            xmlSerial.Serialize(fs, sandclock);
+            fs.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sandclock = new SandClock();
+            if (File.Exists("sandclock.xml"))
+            {
+                fs = new FileStream("sandclock.xml", FileMode.Open);
+                xmlSerial = new XmlSerializer(typeof(SandClock));
+                sandclock = (SandClock)xmlSerial.Deserialize(fs);
+                fs.Close();
+            }
+            textBoxA.Text = sandclock.A.ToString();
+            textBoxB.Text = sandclock.B.ToString();
+        }
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            if (File.Exists("sandclock.dat"))
+            {
+                fs = new FileStream("sandclock.dat", FileMode.Open);
+                using (BinaryReader reader = new BinaryReader(fs))
+                {
+                    double a = reader.ReadDouble();
+                    double b = reader.ReadDouble();
+                    sandclock = new SandClock(a, b);
+                }
+                fs.Close();
+                textBoxA.Text = sandclock.A.ToString();
+                textBoxB.Text = sandclock.B.ToString();
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            sandclock = new SandClock(double.Parse(textBoxA.Text), double.Parse(textBoxB.Text));
+            fs = new FileStream("sandclock.dat", FileMode.OpenOrCreate);
+            using (BinaryWriter writer = new BinaryWriter(fs))
+            {
+                writer.Write(sandclock.A);
+                writer.Write(sandclock.B);
+            }
+            fs.Close();
+        }
+    }
+}
